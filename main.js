6b1dc573be9edfda73dae4262287df57ae88f1e0
houml812 <houml812@gmail.com>
2020-11-24 20:58:33 -0700

    added hovering, changed dropdown to categories, commented code

--- main.js
+++ main.js
@@ -1,191 +1,132 @@
-var width = 1200;
+var width = 1500;
 var height = 800;
 
 d3.csv("starbucksfoods.csv", function (csv) {
 
-    for (var i = 0; i < csv.length; ++i) {
-        csv[i].Calories = Number(csv[i].Calories);
-        csv[i].Fat = Number(csv[i].Fat);
-        csv[i].Carb = Number(csv[i].Carb);
-        csv[i].Fiber = Number(csv[i].Fiber);
-        csv[i].Protein = Number(csv[i].Protein);
-    }
 
-    // csv = csv.sort(function(a, b) {
-    //     return d3.ascending(a.Calories, b.Calories);
-    // });
+    //margin values to format PCP within div
     var margin = {top: 30, right: 10, bottom: 10, left: 0},
-        width = 1200 - margin.left - margin.right,
+        width = 1500 - margin.left - margin.right,
         height = 800 - margin.top - margin.bottom;
 
 
+    //PCP graph
     var graph = d3.select("#pcp").append("svg")
-        // .attr("width", width)
-        // .attr("height", height)
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
-        .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
-    // var graph = d3.select("#barGraph")
-    //     .append("svg")
-    //     .attr("width", width)
-    //     .attr("height", height);
+    //get the 5 axes values (Calories, Fat, Carb, Fiber Protein)
+    var dimensions = d3.keys(csv[0]).filter( function(d) {
+        return (d != "Item" && d != "Category");
+    });
 
-    var dimensions = d3.keys(csv[0]).filter(function(d) {return d != "Item"});
 
-    // For each dimension, I build a linear scale. I store all in a y object
+    //for each dimension, build a linear scale and store in y
     var y = {};
     for (i in dimensions) {
-        var name = dimensions[i];
-        y[name] = d3.scaleLinear()
-            .domain( d3.extent(csv, function(d) { return +d[name]; }) )
+        var dimName = dimensions[i];
+        y[dimName] = d3.scaleLinear()
+            .domain( d3.extent(csv, function(d) { return +d[dimName]; }) )
             .range([height, 0])
     }
 
-    // Build the X scale -> it find the best position for each Y axis
-    x = d3.scalePoint()
+
+    //build "x" scale by distributing the y-axes in equal intervals across width
+    var x = d3.scalePoint()
         .range([0, width])
         .padding(1)
         .domain(dimensions);
 
-    // The path function take a row of the csv as input, and return x and y coordinates of the line to draw for this raw.
+
+    //a function that takes in a row of csv/a starbucks item and returns the (x,y) coordinates of line to draw
     function path(d) {
-        return d3.line()(dimensions.map(function(p) { return [x(p), y[p](d[p])]; }));
+        return d3.line()(dimensions.map( function(p) {
+            return [x(p), y[p](d[p])];
+        }));
     }
 
-    // Draw the lines
+
+    //color mappings for each of the categories
+    var color = d3.scaleOrdinal()
+        .domain(["Bagels", "Bars", "Bowls", "Breakfasts", "Cake Pops", "Cookies", "Croissants", "Desserts",
+            "Muffins & Breads", "Protein Boxes", "Scones", "Salads", "Sandwiches", "Snacks", "Yogurts"])
+        .range(["#000000", "#090979", "#003f5c", "#2f4b7c", "#665191", "#a05195", "#d45087", "#f95d6a", "#ff7c43", "#ffa600",
+            "#bf940b", "#787f18", "#005c2d", "#002f17", "#006b65"]);
+
+
+    //highlights hovered lines/paths
+    var highlight = function(d) {
+        //select the line being hovered and change styling
+        d3.select(this)
+            .transition().duration(200)
+            //color is chosen according to mapping above ****NOTE too many colors?
+            .style("stroke", color(d.Category))
+            .style("stroke-width", 4)
+            .style("opacity", "1");
+    };
+
+
+    //un-highlights lines/paths and returns all to default
+    var unHighlight = function(d) {
+        d3.selectAll(".lines")
+            .transition().duration(200).delay(200)
+            .style("stroke", "#00704A")
+            .style("stroke-width", 1)
+            .style("opacity", 0.6);
+    };
+
+
+    //draws the lines onto the PCP graph
     graph
-        .selectAll("myPath")
+        .selectAll("myPaths")
         .data(csv)
         .enter().append("path")
-        .attr("d",  path)
+        .attr("d", path)
+        .attr("class", "lines")
         .style("fill", "none")
-        .style("stroke", "#69b3a2")
-        .style("opacity", 0.5)
+        //color them all the same ***NOTE too many colors (15 for 15 categories)
+        .style("stroke", "#00704A")
+        .style("opacity", 0.6)
         .on('click', function(d) {
-            console.log(d);
             generatePieChart(d);
-        });
-        // .on("mouseover", function (d, i) {
-        //     d3.select(this).attr("opacity", 1);
-        // })
-        // .on("mouseover", function (d, i) {
-        // d3.select(this).attr("opacity", 0.5);
-        // });
+        })
+        .on("mouseover", highlight)
+        .on("mouseout", unHighlight);
 
-    // Draw the axis:
-    graph.selectAll("myAxis")
-    // For each dimension of the dataset I add a 'g' element:
+
+    //draw the axes
+    graph.selectAll("myAxes")
         .data(dimensions).enter()
+        //each axis is in its own group
         .append("g")
-        // I translate this element to its right position on the x axis
+        //translate the axis to its correct position along the width
         .attr("transform", function(d) { return "translate(" + x(d) + ")"; })
-        // And I build the axis with the call function
-        .each(function(d) { d3.select(this).call(d3.axisLeft().scale(y[d])); })
-        // Add axis title
+        .each( function(d) { d3.select(this).call(d3.axisLeft().scale(y[d])); })
+        .attr("class", "axis")
+        //axis title and styling
         .append("text")
         .style("text-anchor", "middle")
         .attr("y", -9)
-        .text(function(d) { return d; })
-        .style("fill", "black");
+        .text( function(d) { return d; })
+        .style("stroke", "black");
 
 
+    //'Filter Data' button and its functionalities
+    d3.select("#button")
+        .style("border", "1px solid black")
+        .text('Filter Data')
+        //only displays the lines for the selected category
+        .on('click', function() {
+            //************************** FINISH THIS FUNCTION FOR FILTERING ********************************
+            //name of selected category
+            console.log(d3.select("#categorySelect").property("value"));
+        });
 
-    // var bars = graph.append("g");
-    //
-    // var xScale = d3.scaleLinear().range([0, width * 0.6]).domain([0, d3.max(csv, function(d) {
-    //     return d.Calories;
-    // })]);
-    // var yScale = d3.scaleBand().rangeRound([0, height-50], 0.3).domain(csv.map(function(d) {
-    //     return d.Item;
-    // }));
-    //
-    // var yAxis = d3.axisLeft(yScale);
-    // var xAxis = g => g.attr("transform", "translate(250, 20)").call(d3.axisTop(xScale));
-    //
-    // bars.append('g')
-    //     .attr('class', 'y axis')
-    //     .attr('transform', 'translate(250, 0)')
-    //     .call(yAxis);
-    //
-    // bars
-    //     .append("g")
-    //     .attr("class", "x axis")
-    //     .attr("transform", "translate(250," + (height - 50) + ")")
-    //     .call(xAxis);
-    //
-    // bars
-    //     .append("g")
-    //     .selectAll(".bar")
-    //     .data(csv)
-    //     .enter()
-    //     .append("rect")
-    //     .attr("class", "bar")
-    //     .attr("transform", "translate(220, 0)")
-    //     .attr("x", 30)
-    //     .attr("y", function (d) {
-    //         return yScale(d.Item);
-    //     })
-    //     .attr("width", function (d) {
-    //         // console.log(d.Calories);
-    //         // console.log(xScale(d.Calories));
-    //         return xScale(d.Calories);
-    //         // return xScale(getValue(d));
-    //     })
-    //     .attr("height", function (d) {
-    //         return yScale.bandwidth() * 0.8;
-    //     })
-    //     .on('click', function(d) {
-    //         console.log(d)
-    //         generatePieChart(d)
-    //     });
-    //
-    // d3.select("#button")
-    //     .style("border", "1px solid black")
-    //     .text('Filter Data')
-    //     .on('click', function() {
-    //
-    //         bars.selectAll('.bar')
-    //             .filter(function(d) {
-    //                 // this.style.opacity = "100";
-    //                 // var selected = d.frequency >= d3.select("#cutOff").property("value");
-    //                 // if (!selected) {
-    //                 //     this.style.opacity = "0";
-    //                 // }
-    //                 // return selected;
-    //
-    //             })
-    //             .transition()
-    //             .duration(function(d) {
-    //                 return Math.random() * 1000;
-    //             })
-    //             .delay(function(d) {
-    //                 return d.frequency;
-    //             })
-    //             .style('fill', getSelection)
-    //             .attr('width', function(d) {
-    //                 return xScale(d.frequency);
-    //             });
-    //
-    //         //change x-axis scale
-    //
-    //     });
-    //
-    // //sorting
-    //
-    //
-    //
-    // function getValue(d) {
-    //
-    // }
-    //
-    // function getSelection() {
-    //     return d3.select("#nutrientSelect").property("value");
-    // }
 
+    //creates the pie chart of the % make-up of calories per macronutrient
     function generatePieChart(data) {
         const radius = 200
 
